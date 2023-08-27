3616741e894f9710ab781b8be0396ed2e3b4d2dc
Huang-Yuanhang <140174372+Huang-Yuanhang@users.noreply.github.com>
2023-08-27 21:42:48 +0800

    guitars.py

---
+++ practicals_6/guitars.py
@@ -0,0 +1,20 @@
+from guitar import Guitar
+
+guitars = []
+print("My guitars!")
+
+guitar_name = input("Name: ")
+while guitar_name != "":
+
+    guitar_year = int(input("Year: "))
+    guitar_cost = float(input("Cost: $ "))
+
+    guitars.append(Guitar(guitar_name, guitar_year, guitar_cost))
+    guitar_name = input("Name: ")
+max_guitar_length = max([len(guitar.name) for guitar in guitars])
+
+print("These are my guitars:")
+for i, guitar in enumerate(guitars, 1):
+    vintage_string = "(vintage)" if guitar.is_vintage() else ""
+    print(f"Guitar {i}: {guitar.name:>{max_guitar_length}} ({guitar.year}),"
+          f" worth $ {guitar.cost:10,.2f} {vintage_string}")
